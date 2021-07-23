be17eb41f4b7e672f5fcf5c0ac46ff50cc3963c9
Felipe <felipe.portillo.delgado@gmail.com>
2021-07-22 23:33:54 -0300

    correcoes

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -33,43 +33,24 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     return [];
   });
 
-  async function verifyStock(productId: number, amount: number):Promise<boolean> {
-    const stock = (await api.get<Stock>(`/stock/${productId}`)).data;
-    //const amountCart = cart.find((product) => product.id === productId)?.amount || 0;
-    
-    if(amount <= stock.amount) {
-      return true;
-    }
-    return false;
-  }
-
-  async function verifyProduct(productId: number): Promise<boolean> {
-    try{
-        const productExists = (await api.get(`/products/${productId}`)).data;
-    
-        if(!productExists){
-            throw new Error();
-        }
-
-        return true;
-    } catch {
-        throw new Error();
-    }
-  }
-
   const addProduct = async (productId: number) => {
     try {
       // TODO
-      await verifyProduct(productId);
+      const productExists = (await api.get<Product>(`/products/${productId}`)).data;
+    
+      if(!productExists){
+        toast.error('Erro na adição do produto');
+        return;
+      }
 
       const product = (await api.get<Product>(`/products/${productId}`)).data;
 
       const productExistsInCart = cart.find((product) => product.id === productId);
 
       if(productExistsInCart) {
-            const hasStock = await verifyStock(productId, productExistsInCart.amount + 1);
-
-            if(!hasStock) {
+            const stock = (await api.get<Stock>(`/stock/${productId}`)).data;
+        
+            if((productExistsInCart.amount) >= stock.amount) {
                 toast.error('Quantidade solicitada fora de estoque');
                 return;
             }
@@ -108,8 +89,11 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const removeProduct = (productId: number) => {
     try {
       // TODO
-      //await verifyProduct(productId);
-      api.get(`/products/${productId}`);
+      const productExists = cart.find((product) => product.id === productId);
+      if(!productExists) {
+        toast.error('Erro na remoção do produto');
+        return;
+      }
 
       const newCart = cart.filter((product) => product.id !== productId);
 
@@ -132,14 +116,12 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
             return;
         }
 
-        await verifyProduct(productId);
-
-        const hasStock = await verifyStock(productId, amount);
-
-        if(!hasStock) {
+        const stock = (await api.get<Stock>(`/stock/${productId}`)).data;
+        
+        if(amount >= stock.amount) {
             toast.error('Quantidade solicitada fora de estoque');
             return;
-        }
+        }        
 
         const newCart = cart.map((product) => {
             if(product.id === productId) {
